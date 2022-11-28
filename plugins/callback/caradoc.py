ee22e0155fc30b579aaa6712545950a4e8bba2ea
Joël Séguillon <joel.seguillon.ext@ariadnext.com>
2022-11-28 12:13:48 +0100

    add hover tr style

--- plugins/callback/caradoc.py
+++ plugins/callback/caradoc.py
@@ -829,6 +829,10 @@ endif::[]
 ifeval::["{caradoc-theme}" == "dark"]
 +++ <style> a, a:hover { color: #8cb4ff } a:hover {text-decoration: none} </style>+++
 +++ <style> code { background: transparent !important; color: white !important }  .hljs-keyword,.hljs-link,.hljs-literal,.hljs-name,.hljs-symbol{color:#569cd6}.hljs-addition,.hljs-deletion{display:inline-block;width:100%}.hljs-link{text-decoration:underline}.hljs-built_in,.hljs-type{color:#4ec9b0}.hljs-class,.hljs-number{color:#b8d7a3}.hljs-meta-string,.hljs-string{color:#d69d85}.hljs-regexp,.hljs-template-tag{color:#9a5334}.hljs-formula,.hljs-function,.hljs-params,.hljs-subst,.hljs-title{color:#dcdcdc}.hljs-comment,.hljs-quote{color:#57a64a;font-style:italic}.hljs-doctag{color:#608b4e}.hljs-meta,.hljs-meta-keyword,.hljs-tag{color:#9b9b9b}.hljs-template-variable,.hljs-variable{color:#bd63c5}.hljs-attr,.hljs-attribute,.hljs-builtin-name{color:#9cdcfe}.hljs-section{color:gold}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:700}.hljs-bullet,.hljs-selector-attr,.hljs-selector-class,.hljs-selector-id,.hljs-selector-pseudo,.hljs-selector-tag{color:#d7ba7d}.hljs-addition{background-color:var(--vscode-diffEditor-insertedTextBackground,rgba(155,185,85,.2));color:#9bb955}.hljs-deletion{background:var(--vscode-diffEditor-removedTextBackground,rgba(255,0,0,.2));color:red} </style> +++
++++ <style> table:not(.no-border) tbody tr:hover{background-color:rgba(255,255,255,.07)}</style> +++
+endif::[]
+ifeval::["{caradoc-theme}" != "dark"]
++++ <style> table:not(.no-border) tbody tr:hover{background-color:rgba(0,0,0,.07)}</style> +++
 endif::[]
 +++ <style> #header, #content, #footer, #footnotes { max-width: none;} .emoji_table td:nth-child(1n+3), .emoji_table th:nth-child(1n+3) { text-align: center; padding-left: 2px; padding-right: 2px; } </style> +++
 +++ <style> .run_indicator { font-size: 1.5em; text-align: center; } table.no-border, table.no-border > tbody > th, table.no-border > tbody > tr > td, table.no-border > tbody > tr { border-collapse: collapse !important; border: none !important; }</style>+++
